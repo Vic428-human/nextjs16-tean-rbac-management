eb6c4860a95986389a381172b2a786038ea57b80
Vic428-human <z0983195379a@gmail.com>
2026-03-13 08:22:28 +0800

    定義pagination相關的參數來源

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,6 +1,16 @@
 import Sidebar from "../components/sidebar";
 
-export default function UsersPage() {
+export default async function UsersPage({
+    searchParams,
+}: {
+    searchParams: Promise<{ q?: string; page?: string }>;
+}) {
+    // /users?q=book&page=3
+    const params = await searchParams;
+    const q = (params.q ?? "").trim(); // 拿關鍵字
+    const page = Math.max(1, Number(params.page ?? 1)); // 拿當前第幾頁
+    const pageSize = 5; // 每頁幾筆
+    console.log("params", params, q, page, pageSize);
     return <div className="min-h-screen bg-gray-50">
         <Sidebar currentPage="/users" />
         <main className="ml-64 p-8 ">
@@ -15,6 +25,9 @@ export default function UsersPage() {
                 </div>
             </div>
             {/* product table */}
+            <div className="bg-white rounded-lg shadow">
+                123
+            </div>
         </main>
     </div>;
 }
